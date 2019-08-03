efaf75155d74f1145d00e4338c9573b8f2c742b3
OmarZayed77 <zayedomarr@gmail.com>
2019-08-03 02:49:07 +0200

    Add Folder Structure

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default (props) => {
     return (
+        <>
         <h1>App</h1>
+        <Button variant="contained" color="primary">
+            Hello World
+        </Button>
+        </>
     );
 }
